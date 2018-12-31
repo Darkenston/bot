cecbeef0fb7948936f34c904afe06bef89b628b7
Darkenston <pierrehd7@gmail.com>
2018-12-31 01:16:52 +0100

    Quizz

--- index.js
+++ index.js
@@ -42,8 +42,10 @@ var reponses=["266","Les Mongoles","Dionysos","Le Riz","Le Jour","Strife","Aegwy
 
 function selectQuestion() {
 				var numberQuestion=getRandomInt(question.length-1);
-				questionChoosed= question[numberQuestion+1];
-				reponseChoosed=reponses[numberQuestion+1];
+				var numberQuestion2=getRandomInt(question.length-1);
+				var numberQuestionFinal=(numberQuestion+numberQuestion2)/2;
+				questionChoosed= question[numberQuestionFinal];
+				reponseChoosed=reponses[numberQuestionFinal];
 				}
 
 function getRandomInt(max) {
